8cd3e4ada8a21b0c350baed76df38065d89ff0d8
DevExpressExampleBot <fakeEmail>
2022-12-16 18:46:38 +0400

    Source auto update [skip ci]

--- VB/DXSample/Metadata.vb
+++ VB/DXSample/Metadata.vb
@@ -1,12 +1,12 @@
-﻿Imports DevExpress.Mvvm.DataAnnotations
-Imports System
+Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace DXSample
+
     Public Class Metadata
         Implements IMetadataProvider(Of Customer)
 
-        Private Sub IMetadataProviderGeneric_BuildMetadata(ByVal builder As MetadataBuilder(Of Customer)) Implements IMetadataProvider(Of Customer).BuildMetadata
-            builder.Property(Function(x) x.ID).ReadOnly().EndProperty().Property(Function(x) x.FirstName).DisplayName("First Name").MaxLength(40).EndProperty().Property(Function(x) x.LastName).DisplayName("Last Name").MaxLength(40).EndProperty().Property(Function(x) x.FullName).Hidden().EndProperty().Property(Function(x) x.Phone).DisplayName("Phone number").PhoneNumberDataType().EndProperty().Property(Function(x) x.Email).DisplayName("Email").EmailAddressDataType().EndProperty().Property(Function(x) x.BirthDate).DisplayName("Birth date").DateTimeDataType().EndProperty()
+        Private Sub BuildMetadata(ByVal builder As MetadataBuilder(Of Customer)) Implements IMetadataProvider(Of Customer).BuildMetadata
+            PropertyMetadataBuilderExtensions.DateTimeDataType(PropertyMetadataBuilderExtensions.EmailAddressDataType(PropertyMetadataBuilderExtensions.PhoneNumberDataType(builder.Property(Function(x) x.ID).ReadOnly().EndProperty().Property(Function(x) x.FirstName).DisplayName("First Name").MaxLength(40).EndProperty().Property(Function(x) x.LastName).DisplayName("Last Name").MaxLength(40).EndProperty().Property(Function(x) x.FullName).Hidden().EndProperty().Property(Function(x) x.Phone).DisplayName("Phone number")).EndProperty().Property(Function(x) x.Email).DisplayName("Email")).EndProperty().Property(Function(x) x.BirthDate).DisplayName("Birth date")).EndProperty()
             builder.Group("Personal").ContainsProperty(Function(x) x.ID).ContainsProperty(Function(x) x.BirthDate).EndGroup().Group("Name").ContainsProperty(Function(x) x.FirstName).ContainsProperty(Function(x) x.LastName).EndGroup().Group("Contact").ContainsProperty(Function(x) x.Phone).ContainsProperty(Function(x) x.Email).EndGroup()
         End Sub
     End Class
@@ -14,8 +14,8 @@ Namespace DXSample
     Public Class ReviewMetadata
         Implements IMetadataProvider(Of Customer)
 
-        Private Sub IMetadataProviderGeneric_BuildMetadata(ByVal builder As MetadataBuilder(Of Customer)) Implements IMetadataProvider(Of Customer).BuildMetadata
-            builder.Property(Function(x) x.ID).Hidden().EndProperty().Property(Function(x) x.FirstName).Hidden().EndProperty().Property(Function(x) x.LastName).Hidden().EndProperty().Property(Function(x) x.FullName).DisplayName("Full Name").ReadOnly().EndProperty().Property(Function(x) x.Phone).DisplayName("Phone number").PhoneNumberDataType().EndProperty().Property(Function(x) x.Email).DisplayName("Email").EmailAddressDataType().EndProperty().Property(Function(x) x.BirthDate).Hidden().EndProperty()
+        Private Sub BuildMetadata(ByVal builder As MetadataBuilder(Of Customer)) Implements IMetadataProvider(Of Customer).BuildMetadata
+            PropertyMetadataBuilderExtensions.EmailAddressDataType(PropertyMetadataBuilderExtensions.PhoneNumberDataType(builder.Property(Function(x) x.ID).Hidden().EndProperty().Property(Function(x) x.FirstName).Hidden().EndProperty().Property(Function(x) x.LastName).Hidden().EndProperty().Property(Function(x) x.FullName).DisplayName("Full Name").ReadOnly().EndProperty().Property(Function(x) x.Phone).DisplayName("Phone number")).EndProperty().Property(Function(x) x.Email).DisplayName("Email")).EndProperty().Property(Function(x) x.BirthDate).Hidden().EndProperty()
             builder.Group("Name").ContainsProperty(Function(x) x.FullName).EndGroup().Group("Contact").ContainsProperty(Function(x) x.Phone).ContainsProperty(Function(x) x.Email).EndGroup()
         End Sub
     End Class
--- VB/DXSample/ViewModel.vb
+++ VB/DXSample/ViewModel.vb
@@ -1,28 +1,33 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports System
 
 Namespace DXSample
+
     Public Class ViewModel
         Inherits ViewModelBase
 
-        Private Property Customer() As Customer
-        Private Property IsReviewMode() As Boolean
-        Public Property Source() As MetadataExtendedSource
+        Private Property Customer As Customer
+
+        Private Property IsReviewMode As Boolean
+
+        Public Property Source As MetadataExtendedSource
             Get
-                Return GetProperty(Function() Source)
+                Return GetProperty(Function() Me.Source)
             End Get
+
             Set(ByVal value As MetadataExtendedSource)
                 SetProperty(Function() Source, value)
             End Set
         End Property
 
         Public Sub New()
-            Customer = New Customer() With {.FirstName = "Anita", .LastName = "Benson", .BirthDate = New Date(1985, 3, 18), .Phone = "(713) 863 813", .Email = "@gmail.com"}
+            Customer = New Customer() With {.FirstName = "Anita", .LastName = "Benson", .BirthDate = New DateTime(1985, 3, 18), .Phone = "(713) 863 813", .Email = "@gmail.com"}
             Source = New MetadataExtendedSource(Customer, MetadataLocator.Create().AddMetadata(GetType(Customer), GetType(ReviewMetadata)))
             IsReviewMode = True
         End Sub
-        <Command> _
+
+        <Command>
         Public Sub ChangeSource()
             If IsReviewMode Then
                 Source = New MetadataExtendedSource(Customer, MetadataLocator.Create().AddMetadata(GetType(Customer), GetType(Metadata)))
@@ -35,16 +40,23 @@ Namespace DXSample
     End Class
 
     Public Class Customer
-        Public Property ID() As Integer
-        Public Property FirstName() As String
-        Public Property LastName() As String
-        Public ReadOnly Property FullName() As String
+
+        Public Property ID As Integer
+
+        Public Property FirstName As String
+
+        Public Property LastName As String
+
+        Public ReadOnly Property FullName As String
             Get
                 Return String.Format("{0} {1}", FirstName, LastName)
             End Get
         End Property
-        Public Property Phone() As String
-        Public Property Email() As String
-        Public Property BirthDate() As Date
+
+        Public Property Phone As String
+
+        Public Property Email As String
+
+        Public Property BirthDate As Date
     End Class
 End Namespace
